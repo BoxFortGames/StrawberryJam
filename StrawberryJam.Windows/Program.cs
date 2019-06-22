044573e5225c7912a4b128ff0c734e463c3530bb
Jeremy Jorgensen <namatsu@gmail.com>
2019-06-22 10:28:01 -0500

    Project Setup

---
+++ StrawberryJam.Windows/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StrawberryJam.Shared;
+
+namespace StrawberryJam.Windows
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new StrawberryGame())
+                game.Run();
+        }
+    }
+#endif
+}
